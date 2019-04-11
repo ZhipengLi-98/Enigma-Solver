f66b671f7bb967e161a9bab946082a4f04cf2ac2
arsenalahm <clevercake7@gmail.com>
2019-04-11 20:34:15 +0800

    Complete searching rings

--- ring.py
+++ ring.py
@@ -1,5 +1,4 @@
-ciph = "fsayuabiansukdnhbrqwwizqckivsdcxp"
-msg = "secretmessagesentheredatasecutiry"
+from utils import *
 
 rings = {}
 flag = {}
@@ -14,11 +13,34 @@ d,i,c,a,s,d 21, 30, 2, 10, 13
 i,c,a,s,d,i 30, 2, 10, 13, 21
 '''
 
+
+def dfs(nexts, goal, depth):
+    if goal in nexts:
+        print(goal, depth)
+        return True
+    for i in nexts:
+        if depth == 0:
+            for j in alphabet:
+                flag[j] = True
+        if i in mapping.keys() and flag[i]:
+            flag[i] = False
+            if dfs(mapping[i], goal, depth + 1):
+                print(i, depth)
+                if depth > 0:
+                    return True
+
+
 if __name__ == "__main__":
+    mapping = {}
     for i in range(len(msg)):
-        if msg[i] in rings.keys():
-            rings[msg[i]].append((ciph[i], i))
-        else:
-            rings[msg[i]] = [(ciph[i], i)]
-        flag[msg[i]] = True
-    print(rings)
+        if msg[i] in mapping.keys() and ciph[i] not in mapping[msg[i]]:
+            mapping[msg[i]].append(ciph[i])
+        elif msg[i] not in mapping.keys():
+            mapping[msg[i]] = [ciph[i]]
+
+    flag = {}
+
+    for i in mapping.keys():
+        print(i)
+        dfs(mapping[i], i, 0)
+        print()
